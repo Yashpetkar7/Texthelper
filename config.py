a14dc1bd6ce400475a1ece5aff840161b75a876c
Yashpetkar7 <yashpetkar07@gmail.com>
2025-11-06 16:09:08 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6460e6e2-674f-4248-830a-d26a424d2d5d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
